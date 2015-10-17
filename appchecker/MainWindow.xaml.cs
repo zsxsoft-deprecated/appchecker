49f1d130bb571459fad3ce61449c63d00fe78ed8
zsx <zsx@zsxsoft.com>
2015-10-17 13:05:35 +0800

    Complete MainWindow

--- appchecker/MainWindow.xaml.cs
+++ appchecker/MainWindow.xaml.cs
@@ -1,26 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
 
 namespace appchecker
 {
+
+    public class CheckerInfo : INotifyPropertyChanged
+    {
+        private string _AppId;
+        /// <summary>
+        /// Gets or sets App ID
+        /// </summary>
+        public string AppId
+        {
+            get
+            {
+                return _AppId;
+            }
+            set
+            {
+                if (_AppId == value) return;
+                _AppId = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("AppId"));
+            }
+        }
+        private string _PHPPath;
+        /// <summary>
+        /// Gets or sets PHP Path.
+        /// </summary>
+        public string PHPPath
+        {
+            get
+            {
+                return _PHPPath;
+            }
+            set
+            {
+                if (_PHPPath == value) return;
+                _PHPPath = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("PHPPath"));
+            }
+        }
+        private string _ZBPPath;
+        /// <summary>
+        /// Gets or sets Z-BlogPHP Path.
+        /// </summary>
+        public string ZBPPath
+        {
+            get
+            {
+                return _ZBPPath;
+            }
+            set
+            {
+                if (_ZBPPath == value) return;
+                _ZBPPath = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("ZBPPath"));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler h = PropertyChanged;
+            if (h != null)
+                h(this, e);
+        }
+    }
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
+        CheckerInfo Data = new CheckerInfo();
         /// <summary>
         /// 启动控制台
         /// </summary>
@@ -34,13 +89,56 @@ namespace appchecker
         public static extern bool FreeConsole();
         public MainWindow()
         {
+            
             InitializeComponent();
+            txtPHPPath.DataContext = Data;
+            txtAppID.DataContext = Data;
         }
 
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
             AllocConsole();
-            Console.WriteLine("测试");
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo();
+            p.StartInfo.FileName = Data.PHPPath;
+            p.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory() + "\\php";
+            p.StartInfo.Arguments = " checker " + Data.AppId;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.OutputDataReceived += (receivedSender, args) => Console.WriteLine(args.Data);
+            p.BeginOutputReadLine();
+            p.WaitForExit();
+        }
+    
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.DefaultExt = ".exe";
+            ofd.Filter = "PHP Execution File|php.exe";
+            if (ofd.ShowDialog() == true)
+            {
+                if (ofd.FileName != "")
+                {
+                    Data.PHPPath = ofd.FileName;
+                }
+            }
+
+        }
+
+        private void btnBrowseZBP_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog ofd = new System.Windows.Forms.FolderBrowserDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (ofd.SelectedPath != "")
+                {
+                    Data.ZBPPath = ofd.SelectedPath;
+                }
+            }
         }
     }
 }
