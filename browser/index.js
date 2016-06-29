67fee026ef761d0de58bd1797650c71b4e46130b
zsx <zsx@zsxsoft.com>
2016-06-29 16:57:52 +0800

    Electron Renderer

--- browser/index.js
+++ browser/index.js
@@ -1,21 +1,31 @@
 'use strict';
 {
-	const commander = require('commander');
-	const app = require('koa')();
-	const router = require('koa-router')();
-	const packageJson = require('./package.json');
-	commander
-		.version(packageJson.version)
-		.option('-b, --bind', 'Listening port', 30000)
-		.parse(process.argv);
+    const commander = require('commander');
+    const { app, BrowserWindow, ipcMain } = require('electron');
+    const packageJson = require('./package.json');
+    let stdout = {};
 
-	router.post("/", function *(next) {
-		
-	});
+    commander
+        .version(packageJson.version)
+        .option('-u, --url [url]', 'Navigate Url', 'http://localhost/')
+        .option('-r, --code [code]', 'Run Code', "require('electron').ipcRenderer.send('message', {message: 'OK'});require('electron').remote.getCurrentWindow().destroy();")
+        .parse(process.argv);
+
+    ipcMain.on('message', (event, arg) => {
+    	stdout = Object.assign(stdout, arg);
+    });
+    app.on('ready', () => {
+        let win = new BrowserWindow({ show: false });
+        win.on('closed', () => {
+            win = null;
+            process.stdout.write(JSON.stringify(stdout));
+            process.exit();
+        });
+        win.webContents.once('did-finish-load', () => {
+            win.webContents.executeJavaScript(commander.code);
+        });
+        win.loadURL(commander.url);
+    });
 
 
-	app.use(router.routers()).use(router.allowedMethods());
-
-	console.log(`Server started at: 127.0.0.1:${commander.bind}`);
-	app.listen(commander.bind);
 }
---
+++ php/lib/Browser/Runner.php
@@ -0,0 +1,2 @@
+<?php
+namespace AppChecker\Browser;
